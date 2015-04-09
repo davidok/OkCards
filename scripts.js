8d27efa38cbea269a8cd0e50aeb1bc26763484ae
David Ok <davidok@gmail.com>
2015-04-09 16:44:49 -0400

    Nesting 'Read more' items under new div.

--- scripts.js
+++ scripts.js
@@ -6,12 +6,15 @@ for (var i=0; i<guardianHeroinData.length; i++) {
 	var rowData = guardianHeroinData[i];
 	
 		//$() tricking the system into thinking javascript is HTML
+		// wrapping items affected by 'Read more' into new div, moreInfo
 	var template = 	$( '<div class="quoteContainer">'+
 					'<div class="mainQuote">'+rowData.mainQuote+'</div>'+
 					'<div class="readMore">Read more</div>'+
-					'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
-					'<div class="source">'+rowData.source+'</div>'+
-					'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
-					'<div class="location">'+rowData.location+'</div>'+
+					'<div class="moreInfo">'+ 
+						'<div class="wholeQuote">'+rowData.wholeQuote+'</div>'+
+						'<div class="source">'+rowData.source+'</div>'+
+						'<div class="yearsClean">'+rowData.yearsClean+'</div>'+
+						'<div class="location">'+rowData.location+'</div>'+
+					'</div>'+	
 					'</div>');
 }
